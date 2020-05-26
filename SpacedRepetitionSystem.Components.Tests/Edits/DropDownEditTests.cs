b374212fe96f54eb829bcfbc3b4c43f65a6fcbb5
Lukas Reinhardt <ABA101010111010@gmail.com>
2020-05-26 12:37:48 +0200

    - UnitTests DropDownEdit - UnitTests PropertyProxy

---
+++ SpacedRepetitionSystem.Components.Tests/Edits/DropDownEditTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpacedRepetitionSystem.Components.Edits;
+using System.Collections.Generic;
+
+namespace SpacedRepetitionSystem.Components.Tests.Edits
+{
+  /// <summary>
+  /// Testclass for <see cref="TestEdit"/>
+  /// </summary>
+  [TestClass]
+  public sealed class DropDownEditTests
+  {
+    /// <summary>
+    /// Tests that the text changed event is invoked when the value is changed
+    /// </summary>
+    [TestMethod]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "BL0005:Component parameter should not be set outside of its component.", Justification = "<Pending>")]
+    public void SetsValueWhenSelectableValuesIsSetTests()
+    {
+      DropDownEdit edit = new DropDownEdit
+      { SelectableValues = new List<string>() { "test", "test1" }  };
+      Assert.AreEqual("test", edit.Value);
+    }
+  }
+}
---
+++ SpacedRepetitionSystem.Components.Tests/Edits/PropertyProxyTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpacedRepetitionSystem.Components.Edits;
+using SpacedRepetitionSystem.Entities.Entities.Cards;
+
+namespace SpacedRepetitionSystem.Components.Tests.Edits
+{
+  /// <summary>
+  /// Testclass for <see cref="PropertyProxy"/>
+  /// </summary>
+  [TestClass]
+  public sealed class PropertyProxyTests
+  {
+    /// <summary>
+    /// Tests the getter and setter functionality
+    /// </summary>
+    [TestMethod]
+    public void GetterAndSetterTest()
+    {
+      string setterValue = null;
+      static string getter() => "get";
+      void setter(string value) => setterValue = value;
+      PropertyProxy propertyProxy = new PropertyProxy(getter, setter, "test", new Card());
+
+      Assert.AreEqual("get", propertyProxy.Value);
+      propertyProxy.Value = "newValue";
+      Assert.AreEqual("newValue", setterValue);
+    }
+
+    /// <summary>
+    /// Tests that the values set are validated
+    /// </summary>
+    [TestMethod]
+    public void ValidatesValueTest()
+    {
+      bool first = true;
+      Deck deck = new Deck();
+      PropertyProxy propertyProxy = new PropertyProxy(() => deck.Title, (value) => deck.Title = value, nameof(Deck.Title), deck);
+      propertyProxy.Validator = (value, entity) =>
+      {
+        if (first)
+        {
+          first = false;
+          Assert.IsTrue(string.IsNullOrEmpty(value));
+        }
+        else
+          Assert.AreEqual("test", value);
+        Assert.AreSame(deck, entity);
+        return "error";
+      };
+      propertyProxy.Value = "test";
+      Assert.AreEqual("error", propertyProxy.ErrorText);
+    }
+  }
+}
